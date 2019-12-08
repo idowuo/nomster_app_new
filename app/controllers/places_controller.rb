620f874d073cc7f0ce47a949ecd49b6a4f52bc16
Olajire Idowu <oidowu@msn.com>
2019-12-08 23:18:22 +0000

    connectinf=g users to actions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,20 +1,22 @@
 class PlacesController < ApplicationController
-      def index
-            @places = Place.all
-      end
-      
-      def new
-            @place = Place.new
-      end
-      
-      def create
-            Place.create(place_params)
-            redirect_to root_path
-      end
+  before_action :authenticate_user!, only: [:new, :create]
 
-      private
+  def index
+    @places = Place.all
+  end
 
-      def place_params
-            params.require(:place).permit(:name, :description, :address)
-      end
+  def new
+    @place = Place.new
+  end
+
+  def create
+      current_user.places.create(place_params)
+      redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
